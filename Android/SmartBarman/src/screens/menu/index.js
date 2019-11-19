71610c5ecfa9608fe1e520838ee3677e0999094e
lucas <quinteiro.lucas95@gmail.com>
2019-11-19 15:31:41 -0300

    Se agrega la autenticacion por huella digital

--- Android/SmartBarman/src/screens/menu/index.js
+++ Android/SmartBarman/src/screens/menu/index.js
@@ -3,9 +3,12 @@ import {
   StyleSheet,
   View,
   Text,
-  ImageBackground
+  ImageBackground,
 } from 'react-native';
-import ButtonMenu from '../../utils/ButtonMenu'
+import TouchID from 'react-native-touch-id';
+
+import ButtonMenu from '../../utils/ButtonMenu';
+
 const Realm = require('realm');
 
 const styles = StyleSheet.create({
@@ -38,7 +41,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 28,
-    fontFamily:'italic',
+    fontFamily: 'italic',
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
@@ -49,70 +52,103 @@ const styles = StyleSheet.create({
 
 });
 
+const config = {
+  title: 'Autenticación requerida', // Android
+  imageColor: '#efb810', // Android
+  imageErrorColor: '#D8000C', // Android
+  sensorDescription: 'Posicione su huella en el lector', // Android
+  sensorErrorDescription: 'No autorizado', // Android
+  cancelText: 'Cancelar', // Android
+  unifiedErrors: true, // use unified error messages (default false)
+};
 
-const Menu = ({ navigation }) =>{
-  realm = new Realm({ path: 'UserDatabase.realm' });
-  realm.write(() => {        
-      if (
-          realm.objects('Drink').filter((aux)=>aux.name === 'Fernet con COCA')
-          .length == 0
-      ) {
-          realm.create('Drink',{
-            name: 'Fernet con Coca',
-            ingredient1 : 'FERNET',
-            ingredient1Percentage : 30,
-            ingredient2 : 'COCA',
-            graduacionAlc : 39
+const authenticate = (navigation, page) => {
+  TouchID.isSupported(config)
+    .then((supported) => {
+      if (supported) {
+        TouchID.authenticate('Es necesario autenticarse para continuar', config)
+          .then((success) => {
+            navigation.navigate(page);
+          })
+          .catch(() => {
+            console.warn('Error al autenticarse');
           });
+      } else {
+        navigation.navigate(page);
       }
-      if (
-        realm.objects('Drink').filter((aux)=>aux.name === 'Gancia con Sprite')
+    })
+    .catch((error) => {
+      // Failure code
+      console.log(error);
+      navigation.navigate(page);
+    });
+};
+
+const Menu = ({ navigation }) => {
+  realm = new Realm({ path: 'UserDatabase.realm' });
+  realm.write(() => {
+    if (
+      realm.objects('Drink').filter((aux) => aux.name === 'Fernet con COCA')
         .length == 0
     ) {
-        realm.create('Drink',{
-          name: 'Gancia con Sprite',
-          ingredient1 : 'GANCIA',
-          ingredient1Percentage : 30,
-          ingredient2 : 'SPRITE',
-          graduacionAlc : 39
-        });
+      realm.create('Drink', {
+        name: 'Fernet con Coca',
+        ingredient1: 'FERNET',
+        ingredient1Percentage: 30,
+        ingredient2: 'COCA',
+        graduacionAlc: 39,
+      });
     }
-  })
-  
-  
-  return( 
-    <View style={{ flex: 1,backgroundColor:'red' }}>
-      
-      <ImageBackground style={{
-          flex:1,
-          alignItems:'center'
-        }} 
-        source={require('../../../assets/menu_principal.jpg')}>
-        <View style={{flex:0.7}}>
-            <View style={{flex:0.4,justifyContent:'center'}}>
-              <Text style={styles.title}>SmartBarman</Text>
-            </View>
-            <View  style={{flex:0.6,justifyContent:'flex-start',}}>
-              <ButtonMenu  title="Preparar un trago" 
-              onPress={() => { navigation.navigate('Connection'); }}/>
+    if (
+      realm.objects('Drink').filter((aux) => aux.name === 'Gancia con Sprite')
+        .length == 0
+    ) {
+      realm.create('Drink', {
+        name: 'Gancia con Sprite',
+        ingredient1: 'GANCIA',
+        ingredient1Percentage: 30,
+        ingredient2: 'SPRITE',
+        graduacionAlc: 39,
+      });
+    }
+  });
 
-              <ButtonMenu  title="Ver mi estado alcohólico"
-              onPress={() => { navigation.navigate('Records'); }}/>
 
-              <ButtonMenu  title="Agregar Huella"
-              onPress={() => { navigation.navigate(''); }}/>
-            </View>
+  return (
+    <View style={{ flex: 1, backgroundColor: 'red' }}>
+
+      <ImageBackground
+        style={{
+          flex: 1,
+          alignItems: 'center',
+        }}
+        source={require('../../../assets/menu_principal.jpg')}
+      >
+        <View style={{ flex: 0.7 }}>
+          <View style={{ flex: 0.4, justifyContent: 'center' }}>
+            <Text style={styles.title}>SmartBarman</Text>
+          </View>
+          <View style={{ flex: 0.6, justifyContent: 'flex-start' }}>
+            <ButtonMenu
+              title="Preparar un trago"
+              onPress={() => { authenticate(navigation, 'Connection'); }}
+            />
+
+            <ButtonMenu
+              title="Ver mi estado alcohólico"
+              onPress={() => { authenticate(navigation, 'Records'); }}
+            />
+
+          </View>
         </View>
       </ImageBackground>
 
     </View>
 
   );
-}
-Menu.navigationOptions = ({navigation}) => {
-  return({
-    header:null
-  })
-}
+};
+Menu.navigationOptions = ({ navigation }) => ({
+  header: null,
+});
 
 export default Menu;
